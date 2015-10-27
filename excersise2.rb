d97674539cf6c2b39d7ff7599416650d39e05280
Rob Holody <robholody@Robs-MacBook-Pro.local>
2015-10-27 13:43:24 -0400

    Ruby Part One Excersise Two

---
+++ excersise2.rb
@@ -0,0 +1,36 @@
+#How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer onscreen.
+
+# meal_cost = 55
+# tip_percent = 20
+
+# good_tip = meal_cost * tip_percent/100
+
+# puts "A good tip for your meal is $#{good_tip}"
+
+
+# Try adding a string and an integer. What happens? Find a way to convert the integer so that it works and use puts to print the answer onscreen.
+
+# number = 3
+
+# puts "This is a sweet number #{number}"
+
+# Evidently, Ruby is much more than just a calculator, but try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+#harder
+# a = 45628
+# b = 7839
+
+# puts "The result of 45628 multiplied by 7839 is #{a * b}"
+
+# number = 45628 * 7839
+# puts "The result of 45628 multiplied by 7839 is #{number}"
+
+
+# What's the value of the expression = TRUE
+
+(true && false) 
+
+
+
+
+
